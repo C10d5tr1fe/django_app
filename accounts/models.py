db7e6929de57fbdfbf8e1846fd1a5045832306b5
dv.kutuzov <dv.kutuzov@tensor.ru>
2020-07-26 00:58:50 +0300

    add custom User

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,38 @@
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, UserManager
-)
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, UserManager
+
+
+class MyUserManager(BaseUserManager):
+    """
+    User Manager Model
+    """
+
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class MyUser(AbstractBaseUser):
@@ -14,20 +45,20 @@ class MyUser(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     city = models.ForeignKey(
-        'City',
+        'scraping.City',
         on_delete=models.SET_NULL,
         null=True,
         blank=True
     )
     language = models.ForeignKey(
-        'Language',
+        'scraping.Language',
         on_delete=models.SET_NULL,
         null=True,
         blank=True
     )
     send_email = models.BooleanField(default=True)
 
-    objects = UserManager()
+    objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
